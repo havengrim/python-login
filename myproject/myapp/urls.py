02c1466addaa3c5fbca94093ca334e6282e3ac52
user.name: "Angelo Guiao <user.email: "angeloguiao.gci@gmail.com>
2024-03-12 14:14:50 +0800

    login practice

--- myproject/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import register, user_login, home_view
-
-urlpatterns = [
-    path('register/', register, name='register'),
-    path('login/', user_login, name='login'),
-    path('home/', home_view, name='my_home'),
-]
